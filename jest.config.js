55078a31669a5c9496733f595732e0b4eeac1229
Aniket More <aniketavinashmore343@gmail.com>
2022-08-11 15:34:53 +0530

    feat: add test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   testMatch: ['**/*.spec.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.js"
+  ]
 }
 
 module.exports = config
